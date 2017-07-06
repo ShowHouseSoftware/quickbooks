ea05bb83df2579b5b653cf0a37eb2bea4a48cd11
Rene de Kat <renedekat@9lives-development.com>
2017-07-06 10:08:50 +0100

    Added setName method to QBClass builder

--- src/Builders/QBClass.php
+++ src/Builders/QBClass.php
@@ -4,4 +4,14 @@ namespace ReneDeKat\Quickbooks\Builders;
 
 class QBClass extends Builder
 {
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->data['Name'] = $name;
+
+        return $this;
+    }
 }
