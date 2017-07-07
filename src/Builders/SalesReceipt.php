4f1eaf468da6c6953ef655358d30da140be0ddb7
Rene de Kat <renedekat@9lives-development.com>
2017-07-07 10:12:50 +0100

    Added helper methods to SalesReceipt

--- src/Builders/SalesReceipt.php
+++ src/Builders/SalesReceipt.php
@@ -2,6 +2,22 @@
 
 namespace ReneDeKat\Quickbooks\Builders;
 
+use ReneDeKat\Quickbooks\Builders\Traits\HasCustomer;
+use ReneDeKat\Quickbooks\Builders\Traits\Itemizable;
+
 class SalesReceipt extends Builder
 {
+    use HasCustomer, Itemizable;
+
+    /**
+     * @param string $txnDate
+     *
+     * @return $this
+     */
+    public function setTxnDate($txnDate)
+    {
+        $this->data['TxnDate'] = $txnDate;
+
+        return $this;
+    }
 }
