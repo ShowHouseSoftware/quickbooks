fa3c6a2be4012e6bf9e40c4782a2a2824bb1c7ea
René de Kat <info@9lives-development.com>
2017-09-12 09:27:20 +0100

    Update SalesReceipt.php

--- src/Services/SalesReceipt.php
+++ src/Services/SalesReceipt.php
@@ -7,4 +7,18 @@ use ReneDeKat\Quickbooks\Services\Traits\Attachable;
 class SalesReceipt extends Service
 {
     use Attachable;
+    
+    /**
+     * Void an entity.
+     *
+     * @param string $id        Item information.
+     * @param int    $syncToken
+     */
+    public function void($id, $syncToken = 0)
+    {
+        return parent::post($this->getResourceName().'?operation=void', [
+            'Id'        => $id,
+            'SyncToken' => $syncToken,
+        ])->{$this->getEntityName()};
+    }
 }
