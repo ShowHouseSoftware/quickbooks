f3d88f2ba643ef03f0bdb0eaeda9d95b1e665ba8
Adrian Walls <awalls@showhousesoftware.com>
2017-09-19 12:25:47 +0100

    Updated methods in Servcies/Bill.php

--- src/Builders/Bill.php
+++ src/Builders/Bill.php
@@ -2,6 +2,36 @@
 
 namespace ReneDeKat\Quickbooks\Builders;
 
+use ReneDeKat\Quickbooks\Builders\Traits\HasCustomer;
+use ReneDeKat\Quickbooks\Builders\Traits\Itemizable;
+
 class Bill extends Builder
 {
+    use HasCustomer, Itemizable;
+
+    /**
+     * @param string $txnDate
+     *
+     * @return $this
+     */
+    public function setTxnDate($txnDate)
+    {
+        $this->data['TxnDate'] = $txnDate;
+
+        return $this;
+    }
+
+    /**
+     * Set due date.
+     *
+     * @param string $dueDate YYYY-MM-DD
+     *
+     * @return $this
+     */
+    public function setDueDate($dueDate)
+    {
+        $this->data['DueDate'] = $dueDate;
+
+        return $this;
+    }
 }
