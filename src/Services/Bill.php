687153c2bee960911f4db0cee136500c295fe8ea
Rene de Kat <renedekat@9lives-development.com>
2017-09-27 16:08:27 +0100

    Removed redundant delete method

--- src/Services/Bill.php
+++ src/Services/Bill.php
@@ -7,18 +7,4 @@ use ReneDeKat\Quickbooks\Services\Traits\Attachable;
 class Bill extends Service
 {
     use Attachable;
-
-    /**
-     * Void an entity.
-     *
-     * @param string $id        Invoice ID.
-     * @param int    $syncToken
-     */
-    public function delete($id, $syncToken = 0)
-    {
-        return parent::post($this->getResourceName().'?operation=delete', [
-            'Id'        => $id,
-            'SyncToken' => $syncToken,
-        ])->{$this->getEntityName()};
-    }
 }
