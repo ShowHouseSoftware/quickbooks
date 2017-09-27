7b75dfdb205914158f84928d1bc3a9f39e2df8f2
Rene de Kat <renedekat@9lives-development.com>
2017-09-27 15:37:46 +0100

    Added void method to Bill service

--- src/Services/Bill.php
+++ src/Services/Bill.php
@@ -7,4 +7,18 @@ use ReneDeKat\Quickbooks\Services\Traits\Attachable;
 class Bill extends Service
 {
     use Attachable;
+
+    /**
+     * Void an entity.
+     *
+     * @param string $id        Invoice ID.
+     * @param int    $syncToken
+     */
+    public function void($id, $syncToken = 0)
+    {
+        return parent::post($this->getResourceName().'?operation=void', [
+            'Id'        => $id,
+            'SyncToken' => $syncToken,
+        ])->{$this->getEntityName()};
+    }
 }
