2064db0740e552b83d049a269545e5d405f9dc0e
René de Kat <info@9lives-development.com>
2017-10-04 09:39:49 +0100

    Update BillPayment.php

--- src/Builders/BillPayment.php
+++ src/Builders/BillPayment.php
@@ -2,6 +2,10 @@
 
 namespace ShowHouseSoftware\Quickbooks\Builders;
 
+use ShowHouseSoftware\Quickbooks\Builders\Traits\HasVendor;
+use ShowHouseSoftware\Quickbooks\Builders\Traits\Itemizable;
+
 class BillPayment extends Builder
 {
+    use HasVendor, Itemizable;
 }
